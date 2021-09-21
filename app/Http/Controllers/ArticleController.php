22cf2e468ae8883373b5e26d448676af547575ce
ToshikiKawata <jubei.animal@gmail.com>
2021-09-21 21:43:22 +0900

    課題2完了

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::all();
+        return view('articles.index', ['articles' => $articles]);
+    }
+
+
+    public function show($id)
+    {
+        $article = Article::find($id);
+        return view('articles.show', ['article' => $article]);
+    }
+
+    public function create()
+    {
+        return  view('articles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $article = new Article;
+
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->timestamps = false;
+
+        $article->save();
+    }
+}
