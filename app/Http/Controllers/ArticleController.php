42832e7f6eaada411758dad1a444dc887e70bd91
ToshikiKawata <jubei.animal@gmail.com>
2021-09-21 23:37:02 +0900

    課題3完了

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -25,6 +25,12 @@ class ArticleController extends Controller
         return  view('articles.create');
     }
 
+    public function edit($id)
+    {
+        $article = article::find($id);
+        return view('articles.edit', ['article' => $article]);
+    }
+
     public function store(Request $request)
     {
         $article = new Article;
@@ -34,5 +40,29 @@ class ArticleController extends Controller
         $article->timestamps = false;
 
         $article->save();
+        // 登録したらindexに戻る
+        return redirect('/articles');
+    }
+
+    public function update(Request $request, $id) 
+    {
+        // ここはidで探して持ってくる以外はstoreと同じ
+        $article = article::find($id);
+    // 値の用意
+
+    $article->title = $request->title;
+    $article->body = $request->body;
+    $article->timestamps = false;
+
+        // 保存
+        $article->save();
+        // 登録したらindexに戻る
+        return redirect('/articles');
+    }
+    public function destroy($id)
+    {
+        $item = article::find($id);
+        $item->delete();
+        return redirect('/articles');
     }
 }
--- resources/views/Articles/edit.blade.php
+++ resources/views/Articles/edit.blade.php
@@ -4,22 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>論文投稿</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Document</title>
 </head>
 <body>
-    <h1>新規論文投稿</h1>
-    <form action="/articles" method="post">
-    @csrf
+    <form action="/articles/{{ $article->id }}" method="post">
+        @csrf
+        @method('PATCH')
+        <input type="hidden" name="id" value="{{ $article->id }}">
         <p>
             <label for="title">論文タイトル</label>
-            <input type="text" name="title" value="{{ old('title') }}">
+            <input type="text" name="title" value="{{ $article->title }}">
         </p>
         <p>
             <label for="body">本文</label>
-            <textarea name="body">{{ old('body') }}</textarea>
+            <textarea name="body">{{ $article->body }}</textarea>
         </p>
-        <input type="submit" value="投稿">
+        <input type="submit" value="更新">
     </form>
 </body>
 </html>
